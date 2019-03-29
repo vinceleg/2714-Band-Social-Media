a88199aa7e8cf1d96e58ee901c65d8c1f94d0469
Darryl Chen <pchen121@my.bcit.ca>
2019-03-28 18:05:56 -0700

    Darryls new code

--- html/routes/loginroutes.js
+++ html/routes/loginroutes.js
@@ -1,20 +1,19 @@
-var mysql = require('mysql');
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : 'password',
-	database : 'bandSocialMedia'
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Yoonaim12',
+  database : 'bandsocialmedia'
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
 });
 
-connection.connect((err) => {
-	if(err){
-	  console.log('Error connecting to Db');
-	  return;
-	}
-	console.log('Connection established');
-   });
-
-   exports.login = function(req,res){
+exports.login = function(req,res){
     var email= req.body.email;
     var password = req.body.password;
     connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {

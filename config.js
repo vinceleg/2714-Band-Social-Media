2201dd460f4befbab0266a7be2d8753ffbde2a16
Darryl Chen <pchen121@my.bcit.ca>
2019-04-17 16:58:04 -0700

    change config to aws database

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 var config = {
 	database: {
-		host:	  'localhost', 	// database host
+		host:	  'bandsocialmedia.cgxvoqdz5ubf.us-east-2.rds.amazonaws.com', 	// database host
 		user: 	  'root', 		// your database username
 		password: 'password', 		// your database password
 		port: 	  3306, 		// default MySQL port
-		db: 	  'bandSocialMedia' 		// your database name
+		db: 	  'bandSocialMedia' ,		// your database name
+		queueLimit : 0,
+		connectionLimit: 0
 	},
 	server: {
 		host: '127.0.0.1',
